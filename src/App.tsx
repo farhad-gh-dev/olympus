a10e7d62303bb5c47f27ab841d492dbad00703bd
source-architect <source.architect@gmail.com>
2021-05-24 18:27:36 +0430

    git status

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>a react app</h1>
+      <div
+        style={{ width: "200px", height: "200px", border: "1px solid red" }}
+        className="custom-gradient"
+      ></div>
+    </div>
+  );
+}
+
+export default App;
