7f3c737f596fc2e65e769df8c70cd11543536467
source-architect <source.architect@gmail.com>
2021-05-25 23:10:27 +0430

    created loading animation on page load

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 function App() {
   return (
     <div className="App">
-      <h1>a react app</h1>
-      <div
-        style={{ width: "200px", height: "200px", border: "1px solid red" }}
-        className="custom-gradient"
-      ></div>
+      <h1>index page</h1>
     </div>
   );
 }
