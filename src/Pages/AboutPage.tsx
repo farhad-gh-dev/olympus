515d44ecbf91626965c63046e1702ace1dbdf54d
source-architect <source.architect@gmail.com>
2021-07-20 23:44:35 +0430

    create description panel in about page

--- src/Pages/AboutPage.tsx
+++ src/Pages/AboutPage.tsx
@@ -18,9 +18,32 @@ const AboutPage: React.FC = () => {
 
       <Navbar links={navbarLinks} />
 
-      <div className="page-container">
+      <div className="page-content">
         <div className="left-side">
           <h1 className="main-title">about us</h1>
+          <div className="description-panel">
+            <div className="description">
+              <p>
+                this website is a personal project with no commercial use or
+                profit. attribution to the owners of assets used in this project
+                is mentioned on credits page.
+              </p>
+              <br />
+              <br />
+              <p>
+                all rights goes to creator of this project farhad gh, in case of
+                any use of this project attribution is needed.
+              </p>
+            </div>
+            <div className="top-left-frame">
+              <div></div>
+              <div></div>
+            </div>
+            <div className="bottom-right-frame">
+              <div></div>
+              <div></div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
